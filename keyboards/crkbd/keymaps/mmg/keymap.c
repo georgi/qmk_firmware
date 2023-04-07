20f96cb6e2bdd6ad2223f33756f0b31aa3045b5d
Matthias Georgi <matti.georgi@gmail.com>
2023-04-07 16:09:51 +0200

    add corne

--- keyboards/crkbd/keymaps/mmg/keymap.c
+++ keyboards/crkbd/keymaps/mmg/keymap.c
@@ -6,37 +6,33 @@ enum layers { BASE, NAV, SYM, NUM  };
 #define LAYOUT LAYOUT_split_3x6_3
 #define __ KC_NO
 
-#define GUI_A GUI_T(KC_A)
-#define GUI_SCLN GUI_T(KC_SCLN)
-#define SYM_S LT(SYM, KC_S)
-#define SYM_L LT(SYM, KC_L)
-#define NUM_D LT(NUM, KC_D)
-#define NUM_K LT(NUM, KC_K)
-#define NAV_F LT(NAV, KC_F)
+#define MOD_A GUI_T(KC_A)
+#define MOD_SCLN GUI_T(KC_SCLN)
+#define MOD_S ALT_T(KC_S)
+#define MOD_L ALT_T(KC_L)
+#define MOD_D CTL_T(KC_D)
+#define MOD_K CTL_T(KC_K)
+#define MOD_F SFT_T(KC_F)
+#define MOD_J SFT_T(KC_J)
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[BASE] = LAYOUT(
-			KC_ESC, KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P, __ ,
-			__,     GUI_A,    SYM_S,   NUM_D,   NAV_F,   KC_G,    KC_H,    KC_J,    NUM_K,   SYM_L,  GUI_SCLN, __ ,
-			__,     KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, __ ,
-			__,     SFT_T(KC_BSPC), CTL_T(KC_TAB), CTL_T(KC_ENT), SFT_T(KC_SPC), __
-		       ),
-	[NAV] = LAYOUT(
-			__, __ ,      __ ,     __ ,     __ ,     __ ,     __,      __,      __ ,     __,      __, __ ,
-			__, __ ,      __ ,     __ ,     __ ,     __ ,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ESC, __ ,
-			__, __ ,      __ ,     __ ,     __ ,     __ ,     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  RESET, __ ,
-			__ ,   KC_LSFT,  KC_LCTL, KC_RCTL, KC_RSFT, __
-		      ),
-	[SYM] = LAYOUT(
-			__, KC_EXLM, KC_AT,   KC_HASH,  KC_DLR, KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, __ ,
-			__, __ ,     __ ,     __ ,     __ ,     __ ,      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, __ ,
-			__, __ ,     __ ,     __ ,     __ ,     __ ,      KC_TILD, KC_DQT,  __,      __,      __ ,     __ ,
-			__ ,   KC_LSFT,  KC_LCTL, KC_RCTL, KC_RSFT, __
-		      ),
-	[NUM] = LAYOUT(
-			__, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     __ ,
-			__, __ ,      __,      __ ,     __,      __ ,     KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,  __ ,
-			__, __ ,      __ ,      __ ,     __,      __ ,    KC_GRV,  KC_QUOT, __ ,     __ ,     __ ,      __ ,
-			__ ,   KC_LSFT,  KC_LCTL, KC_RCTL, KC_RSFT, __
-		      ),
+    [BASE] = LAYOUT(
+        KC_ESC,  KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P, __ ,
+        KC_LCTL, MOD_A, MOD_S,   MOD_D,   MOD_F,   KC_G,    KC_H,    MOD_J,   MOD_K,   MOD_L,  MOD_SCLN, KC_RCTL,
+        KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        __,     LT(SYM, KC_BSPC), LT(NAV, KC_TAB), LT(NAV, KC_ENT), LT(SYM, KC_SPC), __
+    ),
+    [NAV] = LAYOUT(
+        KC_ESC,  KC_1,  KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, __,
+        KC_LCTL, __,    __,      __,      __,      KC_ESC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, __,   KC_RCTL,
+        KC_LSFT, __,    __,      __,      __,      __,      __,      KC_MINS, KC_EQL,  __,      __,   KC_RSFT,
+        __ , LT(SYM, KC_BSPC), KC_TAB, KC_ENT, LT(SYM, KC_SPC), __
+   ),
+  [SYM] = LAYOUT(
+        KC_ESC,  KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, __,
+        KC_LCTL, KC_TILD,  KC_GRV,  KC_LCBR, KC_RCBR, KC_QUOT, KC_DQT,  KC_LBRC, KC_RBRC, KC_PIPE, KC_BSLS, KC_RCTL,
+        KC_LSFT, __,       __,      __,      __,      __,      __,      KC_UNDS, KC_PLUS, __,      __,      KC_RSFT,
+        __,  KC_BSPC, LT(NAV, KC_TAB), LT(NAV, KC_ENT), KC_SPC, __
+  ),
 };
