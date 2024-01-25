54fcb7949661701695c82d35f71f8f3082471ea6
Matthias Georgi <matti.georgi@gmail.com>
2024-01-25 21:30:51 +0100

    shift lower row

--- keyboards/maple_computing/minidox/keymaps/mmg/keymap.c
+++ keyboards/maple_computing/minidox/keymaps/mmg/keymap.c
@@ -19,21 +19,21 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT(
-    KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,
-    MOD_A,    MOD_S,   MOD_D,   MOD_F,   KC_G,    KC_H,    MOD_J,   MOD_K,   MOD_L,  MOD_SCLN,
-    KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,
-    __,  LT(SYM, KC_BSPC), LT(NAV, KC_TAB), LT(NAV, KC_ENT), LT(SYM, KC_SPC), __
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,
+    MOD_A,   MOD_S,   MOD_D,   MOD_F,   KC_G,    KC_H,    MOD_J,   MOD_K,   MOD_L,  MOD_SCLN,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,
+    KC_ESC,  LT(SYM, KC_BSPC), LT(NAV, KC_TAB), LT(NAV, KC_ENT), LT(SYM, KC_SPC), KC_ESC
   ),
   [NAV] = LAYOUT(
-    KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_ESC,  KC_HOME, KC_PGUP, KC_PGDN, KC_END,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ESC,
-    KC_CAPS, __,      __,      __,      QK_BOOT,  __,      __,      __,      __,      KC_CAPS,
-    __ , LT(SYM, KC_BSPC), KC_TAB, KC_ENT, LT(SYM, KC_SPC), __
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+    __,      KC_HOME, KC_PGUP, KC_PGDN, KC_END,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, __,
+    KC_CAPS, __,      __,      __,      QK_BOOT,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
+    KC_ESC, KC_BSPC, KC_TAB, KC_ENT, KC_SPC, KC_ESC
   ),
   [SYM] = LAYOUT(
     KC_EXLM, KC_AT,   KC_HASH,  KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
     KC_TILD, KC_GRV,  KC_LCBR,  KC_RCBR, KC_QUOT, KC_DQT,  KC_LBRC, KC_RBRC, KC_PIPE, KC_BSLS,
-    __,      __,      __,       __,      __,      KC_UNDS, KC_MINS, KC_PLUS,   KC_EQL,    __,
-    __,  KC_BSPC, LT(NAV, KC_TAB), LT(NAV, KC_ENT), KC_SPC, __
+    __,      __,      __,       __,      __,      __,      KC_UNDS, KC_MINS, KC_PLUS, KC_EQL,
+    KC_ESC,  KC_BSPC, KC_TAB, KC_ENT, KC_SPC, KC_ESC
   ),
 };
